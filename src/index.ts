67fe66c9311f652dd759b2216a0dc26dade13885
Hemp <hemp@hey.com>
2023-07-05 10:41:44 -0500

    wip

--- src/index.ts
+++ src/index.ts
@@ -2,8 +2,8 @@ import './index.css'
 import Layout from './components/Layout.vue'
 import type { Theme } from 'vitepress'
 
-const LNTheme: Theme = { Layout }
+const PilgrimTheme: Theme = { Layout }
 
-export { LNTheme as Theme }
+export { PilgrimTheme as Theme }
 
 export type { ThemeConfig } from './config'
