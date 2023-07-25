56931579bf08ff297162f145e84e7ac066fe00bb
Hemp <hemp@hey.com>
2023-07-25 10:24:45 -0500

    wip

--- tailwind.config.js
+++ tailwind.config.js
@@ -3,12 +3,17 @@ const colors = require('tailwindcss/colors')
 
 /** @type {import('tailwindcss').Config} */
 module.exports = {
-  content: ['./src/**/*.{html,js,vue,ts}', './node_modules/pilgrim-theme/src/**/*.{html,js,vue,ts}'],
+  content: [
+    './src/**/*.{html,js,vue,ts}',
+    './node_modules/pilgrim-theme/src/**/*.{html,js,vue,ts}',
+  ],
   darkMode: 'class',
   theme: {
     extend: {
       colors: { gray: colors.slate, blue: colors.sky, primary: colors.sky },
-      fontFamily: { sans: ['Nunito Sans', ...defaultTheme.fontFamily.sans] },
+      fontFamily: {
+        sans: ['Figtree', ...defaultTheme.fontFamily.sans],
+      },
     },
   },
   safelist: ['table-of-contents'],
