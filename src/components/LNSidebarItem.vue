35ac9b0367ec8a4fc0f5cd61a82ad3a94a64d072
Mior Muhammad Zaki <crynobone@gmail.com>
2024-05-20 16:10:53 +0800

    Allow multiple sidebars and fixed light background.
    Signed-off-by: Mior Muhammad Zaki <crynobone@gmail.com>

--- src/components/LNSidebarItem.vue
+++ src/components/LNSidebarItem.vue
@@ -1,6 +1,7 @@
 <script setup lang="ts">
 import { useSidebarControl } from '../composables/sidebar'
 import { computed } from 'vue'
+import { withBase } from 'vitepress'
 import type { SidebarLink } from '../types'
 
 const props = defineProps<{
@@ -8,10 +9,11 @@ const props = defineProps<{
 }>()
 
 const { isActiveLink } = useSidebarControl(computed(() => props.item))
+const link = computed(() => withBase(props.item.link))
 </script>
 
 <template>
-  <a :href="item.link"
+  <a :href="link"
     class="text-sm text-gray-500 hover:text-gray-600 border-l border-gray-300/50 dark:border-gray-800 flex items-start pl-4 py-1 hover:border-primary-500 dark:hover:border-gray-700"
     :class="{
       '!text-primary-500 !border-primary-500': isActiveLink,
